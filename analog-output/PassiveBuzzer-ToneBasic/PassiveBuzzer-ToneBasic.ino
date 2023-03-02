e79dd4691bfd5d57cdaac6026f732e5c44ea7c4c
Michael Rivera <mrivera.lee@gmail.com>
2023-03-02 14:56:31 -0700

    update piezo buzzer

--- analog-output/PassiveBuzzer-ToneBasic/PassiveBuzzer-ToneBasic.ino
+++ analog-output/PassiveBuzzer-ToneBasic/PassiveBuzzer-ToneBasic.ino
@@ -5,13 +5,23 @@
 const int PIEZO_PIN = 11;
 
 void setup() {
-  // do nothing for pwm pins
+  pinMode(PIEZO_PIN, OUTPUT);
 }
 
 void loop() {
   // frequency (in hertz) and duration (in milliseconds).
-  int frequencyHz = 100; // could be any pitch from pitches.h
+  int frequencyHz = 100; // could aslo be any pitch from pitches.h
+
+  // duration in milliseconds of the tone
   int durationMs = 90;
+
+  // Two ways to use tone:
+  // First plays a tone continuously (and indefinitely until you specify turning it off)
+  tone(PIEZO_PIN, frequencyHz);
+  // To turn off you use the "noTone(pin) function
+  noTone(PIEZO_PIN);
+
+  // Otherway is to specify a duration in the call to tone like so:
   tone(PIEZO_PIN, frequencyHz, durationMs);
   delay(100);
 }
